33a52806e051f471a77d42327fb893138429c695
Mihail <gusev.mihail.34@gmail.com>
2019-01-30 10:21:47 +0300

    Тестовый вывод данных в таблицу

--- Application.py
+++ Application.py
@@ -1,4 +1,5 @@
-# from pathlib import Path
+import os
+
 import openpyxl
 
 import Parser
@@ -6,13 +7,21 @@ import Pre_processing
 import Table_generator
 
 if __name__ == "__main__":
-    workbook_asu = openpyxl.load_workbook(r'.\input data\5. Графики на 05.18 АСУ.xlsx')
+    # workbook = openpyxl.load_workbook(r'.\input data\5. Графики на 05.18 АСУ.xlsx')
+    workbook = openpyxl.load_workbook(r'.\input data\Test Schedule.xlsx')
     jobs = []
-    for sheet_name in workbook_asu.sheetnames:  # find necessary worksheets by names
+    for sheet_name in workbook.sheetnames:  # find necessary worksheets by names
         system = Pre_processing.extract_system(sheet_name)
         if system is not None:
-            parser = Parser.ParserAsu(workbook_asu[sheet_name])
+            parser = Parser.ParserAsu(workbook[sheet_name])
             Pre_processing.parser_to_jobs(parser, jobs)
     # for job in jobs:
     #     print(job)
+    template_filename = r'.\input data\Template.xlsx'
+    output_filename = r'test_wb.xlsx'
+    test_table = Table_generator.WorkPlan(jobs, template_filename)
+    test_table._write_obj_row('somwhere in far far away damba')
+    test_table.make_plan()
+    test_table.save_file(output_filename)
 
+    os.startfile(output_filename)  # debug
--- Table_generator.py
+++ Table_generator.py
@@ -1,98 +1,98 @@
 from datetime import date
 import os  # debug
+from typing import List
 
 import openpyxl
-from openpyxl.styles import Font, Border, Side
+from openpyxl.styles import Font, Border, Side, Alignment
 from openpyxl.worksheet import Worksheet
 
 from Cell_styler import apply_style, TableArea
-
-FIRST_COL = 1
-LAST_COL = 9
-FIRST_DATA_ROW = 10
-
-thin_side = Side(style='thin', color='000000')
-thin_border = Border(left=thin_side, top=thin_side, right=thin_side, bottom=thin_side)
-basic_font = Font(name='Times New Roman', size=11)
-bold_font = Font(name='Times New Roman', size=11, b=True)
-
-current_row = FIRST_DATA_ROW
+from Pre_processing import Job
 
 
-# ws = openpyxl.load_workbook(r'.\input data\11.12.18.xlsx')['Лист1']
-# col_dim = ws.column_dimensions
-# first_col = col_dim['A']
-# col_width = first_col.width
-# print(ws.column_dimensions['A'].width)
-def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
-    """
-    Apply styles to a range of cells as if they were a single cell.
+class WorkPlan:
 
-    :param ws:  Excel worksheet instance
-    :param range: An excel range to style (e.g. A1:F20)
-    :param border: An openpyxl Border
-    :param fill: An openpyxl PatternFill or GradientFill
-    :param font: An openpyxl Font object
-    """
+    def __init__(self, jobs: List[Job], template_path: str):
+        self.jobs = jobs
+        self._first_col = None
+        self._last_col = None
+        self._first_data_row = None
+        self._current_row = None
+        self._thin_side = Side(style='thin', color='000000')
+        self._thin_border = Border(left=self._thin_side, top=self._thin_side,
+                                   right=self._thin_side, bottom=self._thin_side)
+        self._basic_font = Font(name='Times New Roman', size=11)
+        self._bold_font = Font(name='Times New Roman', size=11, b=True)
+        self._align_center = Alignment(horizontal='center', vertical='center')
+        self._wb = self._get_template(template_path)
+        self._ws = self._wb.active
+        self._write_date()
 
-    top = Border(top=border.top)
-    left = Border(left=border.left)
-    right = Border(right=border.right)
-    bottom = Border(bottom=border.bottom)
+    def _get_template(self, filename: str):
+        wb_template = openpyxl.load_workbook(filename)
+        ws_template = wb_template.active
+        self._first_col = 1
+        self._last_col = 9
+        self._first_data_row = 10
+        self._current_row = self._first_data_row
+        table_header = TableArea(first_row=8, last_row=9, first_col=self._first_col, last_col=self._last_col)
+        apply_style(ws_template, table_header, border=self._thin_border)
+        return wb_template
 
-    first_cell = ws[cell_range.split(":")[0]]
-    if alignment:
-        ws.merge_cells(cell_range)
-        first_cell.alignment = alignment
+    def _write_date(self):
+        date_cell = self._ws.cell(row=6, column=1)
+        date_cell.value = self.jobs[0].date
 
-    rows = ws[cell_range]
-    if font:
-        first_cell.font = font
+    def _write_obj_row(self, obj_name):
+        self._ws.merge_cells(start_row=self._current_row, end_row=self._current_row,
+                             start_column=self._first_col, end_column=self._last_col)
 
-    for cell in rows[0]:
-        cell.border = cell.border + top
-    for cell in rows[-1]:
-        cell.border = cell.border + bottom
+        current_row_area = TableArea(self._current_row, self._first_col, self._current_row, self._last_col)
+        apply_style(self._ws, current_row_area, border=self._thin_border, font=self._bold_font)
+        self._ws.cell(self._current_row, self._first_col).value = obj_name
+        self._current_row += 1
 
-    for row in rows:
-        l = row[0]
-        r = row[-1]
-        l.border = l.border + left
-        r.border = r.border + right
-        if fill:
-            for c in row:
-                c.fill = fill
+    def _write_work_row(self, job: Job):
+        current_row_area = TableArea(self._current_row, self._first_col, self._current_row, self._last_col)
+        apply_style(self._ws, current_row_area,
+                    border=self._thin_border,
+                    font=self._basic_font,
+                    alignment=self._align_center)
+        organization_col = 1
+        organization = 'Би. Си. Си. ООО'
+        system_col = 2
+        work_col = 3
+        place_col = 4
+        work_start_col = 5
+        work_start = '9:00'
+        work_end_col = 6
+        work_end = '18:00'
+        worker_col = 7
+        job.worker = 'Подольский Андрей Вениаминович +79522726122'  # temporary code for test
+        self._ws.cell(self._current_row, organization_col).value = organization
+        self._ws.cell(self._current_row, system_col).value = job.system
+        self._ws.cell(self._current_row, work_col).value = job.work_type
+        self._ws.cell(self._current_row, place_col).value = job.place
+        self._ws.cell(self._current_row, work_start_col).value = work_start
+        self._ws.cell(self._current_row, work_end_col).value = work_end
+        self._ws.cell(self._current_row, worker_col).value = job.worker
 
+    def make_plan(self):
+        for job in self.jobs:
+            self._write_work_row(job)
 
-def get_template(path: str):
-    wb_template = openpyxl.load_workbook(path)
-    ws_template = wb_template.active
-    header_1 = TableArea(first_row=1, last_row=7, first_col=FIRST_COL, last_col=LAST_COL)
-    header_2 = TableArea(first_row=8, last_row=9, first_col=FIRST_COL, last_col=LAST_COL)
-    apply_style(ws_template, header_2, border=thin_border)
-    return wb_template
-
-
-def write_date(ws: Worksheet, table_date: date):
-    date_cell = ws.cell(row=6, column=1)
-    date_cell.value = table_date
-
-
-def write_obj_row(ws: Worksheet, obj_name: str):
-    ws.merge_cells(start_row=current_row, end_row=current_row, start_column=FIRST_COL, end_column=LAST_COL)
-    apply_style(ws, TableArea(first_row=current_row, last_row=current_row, first_col=FIRST_COL, last_col=LAST_COL),
-                border=thin_border, font=bold_font)
-    ws.cell(current_row, FIRST_COL).value = obj_name
+    def save_file(self, filename):
+        self._wb.save(filename)
 
 
 if __name__ == '__main__':
     # test_out_wb = openpyxl.Workbook()
-    template_filename = r'.\input data\Template.xlsx'
-    test_out_wb = get_template(template_filename)
-    test_ws = test_out_wb.worksheets[0]
-    test_date = date(2019, 1, 20)
-    write_date(test_ws, test_date)
-    write_obj_row(test_ws, 'TEST')
+    # template_filename = r'.\input data\Template.xlsx'
+    # test_out_wb = get_template(template_filename)
+    # test_ws = test_out_wb.worksheets[0]
+    # test_date = date(2019, 1, 20)
+    # write_date(test_ws, test_date)
+    # write_obj_row(test_ws, 'TEST')
 
     # style1 = NamedStyle(name='Style1')
     # style1.border = Border(left=thin_side, top=thin_side, right=thin_side, bottom=thin_side)
@@ -101,5 +101,5 @@ if __name__ == '__main__':
     # test_out_wb.add_named_style(style1)
 
     dest_filename = r'test_wb.xlsx'
-    test_out_wb.save(filename=dest_filename)
-    os.startfile(dest_filename)  # debug
+    # test_out_wb.save(filename=dest_filename)
+    # os.startfile(dest_filename)  # debug
