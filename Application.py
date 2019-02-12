3d3773e74f44ba6ec252e7cb72ba720614299f99
Mihail <gusev.mihail.34@gmail.com>
2019-02-12 16:14:36 +0300

    Рефакторинг кода по обработке входных файлов и сохранению результирующих.

--- Application.py
+++ Application.py
@@ -25,18 +25,29 @@ def find_sheets_asu(wb: openpyxl.Workbook) -> Tuple[openpyxl.workbook.workbook.W
 def find_sheets_vols(wb: openpyxl.Workbook) -> Tuple[openpyxl.workbook.workbook.Worksheet]:
     return tuple(sheet for sheet in wb.worksheets if 'ТО' in sheet.title and sheet.sheet_state == 'visible')
 
-def parse_asu_folder():
+
+def process_files(folder: str, find_sheets_function, parser_class):
     jobs_list: List[Pre_processing.Job] = []
-    path_asu = r'.\input data\АСУ'
-    for file in get_xlsx_files(path_asu):
+    for file in get_xlsx_files(folder):
         print(f'parse asu file: {file}')
-        file_path = path_asu + '\\' + str(file)
-        workbook_asu = openpyxl.load_workbook(file_path)
-        for sheet_name in workbook_asu.sheetnames:  # find necessary worksheets by names
-            system = Pre_processing.extract_system(sheet_name)
-            if system is not None:
-                parser = Parser.ParserAsu(workbook_asu[sheet_name])
-                Pre_processing.parser_to_jobs(parser, jobs_list)
+        file_path = folder + '\\' + str(file)
+        workbook = openpyxl.load_workbook(file_path)
+        sheets = find_sheets_function(openpyxl.load_workbook(file_path))
+        for sheet in sheets:
+            parser = parser_class(sheet)
+            jobs_list.extend(Pre_processing.parser_to_jobs(parser))
+    return jobs_list
+
+
+def make_xlsx_from_jobs(jobs_list):
+    jobs.sort(key=lambda job: (job.date, job.object, job.system, job.work_type))
+    jobs_by_days = groupby(jobs, key=lambda job: job.date)
+    for j in jobs_by_days:
+        template_filename = r'.\input data\Template.xlsx'
+        day_job = list(j[1])
+        test_table = Table_generator.WorkPlan(day_job, template_filename)
+        test_table.make_plan()
+        test_table.save_file()
 
 
 if __name__ == "__main__":
@@ -44,7 +55,10 @@ if __name__ == "__main__":
     workbook_test_vols = openpyxl.load_workbook(r'.\input data\Test Schedule VOLS.xlsx')
 
     jobs = []
-    jobs.extend(parse_asu_folder())
+    jobs.extend(process_files(r'.\input data\АСУ', find_sheets_asu, Parser.ParserAsu))
+    print(len(jobs))
+
+    # print(find_sheets_vols(workbook_test_vols))
 
     # make_xlsx_from_jobs(jobs)
     print('Генерация успешно завершена')
