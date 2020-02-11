8844c5c6d21ab77b11171f5641626b67a3952848
Mihail <gusev.mihail.34@gmail.com>
2020-02-11 17:04:55 +0300

    Генерация всех журналов

--- config_journals.py
+++ config_journals.py
@@ -11,11 +11,12 @@ class PFilters(Enum):
     north = 'Север'
 
 
-batch_ASU_journals = {'АСУ ТП С1 Север': JournalGeneratorConfig(Systems.ASU_TP, Objects.S1, PFilters.north),
-                      'АСУ ТП С1 Юг': JournalGeneratorConfig(Systems.ASU_TP, Objects.S1, PFilters.north),
-                      'АСУ ТП С2 Север': JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, PFilters.north),
-                      'АСУ ТП С2 Юг': JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, PFilters.north),
-                      # 'АСУ ТП С2 АМ':       JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, PFilters.north),
+default_header_ASU = (('Дата', 10), ('Место', 30), ('Тип', 7), ('Тех. карта', 50), ('Исполнитель', 15))
+
+batch_ASU_journals = {'АСУ ТП С1 Север': JournalGeneratorConfig(Systems.ASU_TP, Objects.S1, 'Север'),
+                      'АСУ ТП С1 Юг': JournalGeneratorConfig(Systems.ASU_TP, Objects.S1, 'Юг'),
+                      'АСУ ТП С2 Север': JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, 'Север'),
+                      'АСУ ТП С2 Юг': JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, 'Юг'),
                       'АСУ ТП В1': JournalGeneratorConfig(Systems.ASU_TP, Objects.V1, None),
                       'АСУ ТП В2': JournalGeneratorConfig(Systems.ASU_TP, Objects.V2, None),
                       'АСУ ТП В3': JournalGeneratorConfig(Systems.ASU_TP, Objects.V3, None),
@@ -24,13 +25,22 @@ batch_ASU_journals = {'АСУ ТП С1 Север': JournalGeneratorConfig(Syste
                       'АСУ ТП В6': JournalGeneratorConfig(Systems.ASU_TP, Objects.V6, None),
                       'АСУ ТП ЗУ': JournalGeneratorConfig(Systems.ASU_TP, Objects.ZU, None),
 
-                      'АСУ И С1 Север': JournalGeneratorConfig(Systems.ASU_I, Objects.S1, PFilters.north),
-                      'АСУ И С1 Юг': JournalGeneratorConfig(Systems.ASU_I, Objects.S1, PFilters.north),
-                      'АСУ И С2 Север': JournalGeneratorConfig(Systems.ASU_I, Objects.S2, PFilters.north),
-                      'АСУ И С2 Юг': JournalGeneratorConfig(Systems.ASU_I, Objects.S2, PFilters.north),
-                      # 'АСУ ТП С2 АМ':       JournalGeneratorConfig(Systems.ASU_TP, Objects.S2, PFilters.north),
-                      'АСУ И ЗУ': JournalGeneratorConfig(Systems.ASU_I, Objects.ZU, None)
+                      'АСУ АМ С2': JournalGeneratorConfig(Systems.ASU_AM, Objects.S2, None),
 
-                      # 'ЛВС':                JournalGeneratorConfig(Systems.LVS, None, None)
+                      'АСУ И С1 Север': JournalGeneratorConfig(Systems.ASU_I, Objects.S1, 'Север'),
+                      'АСУ И С1 Юг': JournalGeneratorConfig(Systems.ASU_I, Objects.S1, 'Юг'),
+                      'АСУ И С2 Север': JournalGeneratorConfig(Systems.ASU_I, Objects.S2, 'Север'),
+                      'АСУ И С2 Юг': JournalGeneratorConfig(Systems.ASU_I, Objects.S2, 'Юг'),
+                      'АСУ И ЗУ': JournalGeneratorConfig(Systems.ASU_I, Objects.ZU, None),
 
+                      'ЛВС': JournalGeneratorConfig(Systems.LVS, None, None),
+                      'ВОЛС': JournalGeneratorConfig(Systems.VOLS, None, None),
+                      'ТК М2': JournalGeneratorConfig(Systems.TK, None, None)
                       }
+
+default_header_ASKUE = (
+('Дата', 10), ('Место', 30), ('Оборудование', 30), ('Тип', 7), ('Тех. карта', 32), ('Исполнитель', 15))
+
+batch_ASKUE_journals = {'АИИСКУЕ': JournalGeneratorConfig(Systems.ASKUE, None, None),
+                        'Тех Учет': JournalGeneratorConfig(Systems.TECH_REG, None, None)
+                        }
--- journal.py
+++ journal.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
-from typing import List, Union, Type
+from typing import List, Tuple, Union, Type
 
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.styles import Alignment
 import pandas as pd
 
 from pre_processing import Job, Systems, Objects
@@ -19,45 +21,80 @@ class Journal(metaclass=ABCMeta):
     def __init__(self, jobs_df: pd.DataFrame):
         self.df = jobs_df
         self.journal: Union[None, pd.DataFrame] = None
+        self.default_header = None
 
     @abstractmethod
     def make_journal(self, sys: Systems, obj: Objects, place: str):
         raise NotImplementedError
 
-    def save_journal(self, file_name='default', folder=r'./output data/journals/'):
+    def save_journal(self, file_name='default', folder=r'./output data/journals/', header='default'):
         if self.journal is None:
             raise ValueError('make journal before save it!')
         else:
             full_file_name = folder + self.df.date[0].strftime('%y %m ') + file_name + '.xlsx'
             sheet_name = 'Sheet1'
+            if header == 'default':
+                header = self.default_header
+            header_names = tuple(col[0] for col in header)
+            col_widths = tuple(col[1] for col in header)
             with pd.ExcelWriter(path=full_file_name, date_format='DD.MM.YY') as writer:
-                header = ('Дата', 'Место', 'Тип', 'Тех. карта', 'Исполнитель')
-                self.journal.to_excel(writer, sheet_name, header=header, index=False)
-                self._set_columns_width(writer.sheets[sheet_name])
+                self.journal.to_excel(writer, sheet_name, header=header_names, index=False)
+                self._set_columns_width(writer.sheets[sheet_name], col_widths)
+                self._set_wrap_text(writer.sheets[sheet_name], (2,))
+
+    def _set_wrap_text(self, sheet: Worksheet, col_numbers: Tuple[int]):
+        table_last_row = self.journal.shape[0] + 1
+        for row in range(1, table_last_row + 1):
+            for col in col_numbers:
+                # print(row, col)
+                sheet.cell(row, col).alignment = Alignment(wrap_text=False)
 
     @staticmethod
-    def _set_columns_width(worksheet):
+    def _set_columns_width(worksheet, col_widths):
         from openpyxl.utils import get_column_letter
-        col_widths = (10, 15, 7, 50, 15)
         for i, width in enumerate(col_widths):
             worksheet.column_dimensions[get_column_letter(i + 1)].width = width
 
 
 class JournalASU(Journal):
-    def make_journal(self, sys: Systems, obj: Objects, place_filter: str = None):
-        df = self.df  # сокращение записи
-        journal = df[df.system == sys] \
-            [df.object == obj] \
+    def __init__(self, jobs_df: pd.DataFrame):
+        super().__init__(jobs_df)
+        from config_journals import default_header_ASU
+        self.default_header = default_header_ASU
+
+    def make_journal(self, sys: Systems, obj: Objects = None, place_filter: str = None):
+        if obj:
+            journal = self.df[self.df.object == obj]
+        else:
+            journal = self.df
+        journal = journal[journal.system == sys] \
             .drop(['system', 'object'], axis=1) \
             .reindex(columns=['date', 'place', 'work_type', 'tech_map', 'performer']) \
             .sort_values(by=['date', 'place', 'work_type'])
-
         if place_filter:
             journal = journal[journal.place.str.contains(place_filter)]
-
-        # journal.date = journal.date.apply(lambda d: d.strftime('%d.%m.%y'))
         journal.performer = journal.performer.apply(lambda w: w.last_name)
+        self.journal = journal
 
+
+class JournalASKUE(Journal):
+    def __init__(self, jobs_df: pd.DataFrame):
+        super().__init__(jobs_df)
+        from config_journals import default_header_ASKUE
+        self.default_header = default_header_ASKUE
+
+    def make_journal(self, sys: Systems, obj: Objects = None, place_filter: str = None):
+        if obj:
+            journal = self.df[self.df.object == obj]
+        else:
+            journal = self.df
+        journal = journal[journal.system == sys] \
+            .drop(['object'], axis=1) \
+            .reindex(columns=['date', 'place', 'equip_name', 'work_type', 'tech_map', 'performer']) \
+            .sort_values(by=['date', 'place', 'work_type'])
+        if place_filter:
+            journal = journal[journal.place.str.contains(place_filter)]
+        journal.performer = journal.performer.apply(lambda w: w.last_name)
         self.journal = journal
 
 
@@ -72,4 +109,4 @@ def batch_journal_generator(jobs_df: pd.DataFrame, journal_class: Type[Journal],
             journals[name] = j
         if save_journals:
             j.save_journal(name)
-        return journals
+    return journals
