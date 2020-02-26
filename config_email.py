f95206b4917f2c163e86e80d30ad7cef92b9ed78
Mihail <gusev.mihail.34@gmail.com>
2020-02-26 19:27:49 +0300

    Изменения конфига по рассылке журналов

--- config_email.py
+++ config_email.py
@@ -8,7 +8,7 @@ script_email_password = 'kzsasu123456'
 
 batch_sending_journals = {'EKushmilev@bcc.ru': ('АСУ И С1',),
                           'NMulin@bcc.ru': ('АСУ ТП С1',),
-                          'AKapritsa@bcc.ru': ('С2', 'АМ', 'ЗУ'),
+                          # 'AKapritsa@bcc.ru': ('С2', 'АМ', 'ЗУ'),
                           'VMakarov@bcc.ru': ('С2', 'АМ', 'ЗУ'),
                           'MKokoev@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
                           'APodolskij@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
