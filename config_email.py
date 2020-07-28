31d587c3e4178caca0a1b6fa4537f38a7f6be2d0
Mihail <gusev.mihail.34@gmail.com>
2020-07-28 15:24:46 +0300

    Изменение конфига рассылки

--- config_email.py
+++ config_email.py
@@ -1,5 +1,6 @@
 script_email_addr = 'kzs.asu.504@gmail.com'  # Отправитель
-script_email_password = 'kzsasu123456'
+script_email_password = 'lxrhxzgwqurnvyqo'
+# script_email_password = 'kzsasu123456'
 
 # Test config
 # batch_sending_journals = {'MGusev@bcc.ru': ('АСУ И С1',),
@@ -12,7 +13,7 @@ batch_sending_journals = {'EKushmilev@bcc.ru': ('АСУ И С1',),
                           'VMakarov@bcc.ru': ('С2', 'АМ', 'ЗУ'),
                           'MKokoev@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
                           'APodolskij@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
-                          'VSanzhara@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
+                          'VSanzhara@bcc.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6', 'АИИСКУЕ'),
                           'AIlin@bcc.ru': ('ЛВС', 'ВОЛС', 'АИИСКУЕ'),
                           'AYastrebov@bcc.ru': ('Тех',),
                           'AOgorodnikov@bcc.ru': ('ТК',)
