a4e7ed91ea44f77be222d7723455163ed68e0cba
Mihail <gusev.mihail.34@gmail.com>
2021-10-08 16:27:41 +0300

    обновление конфигов

--- config_email.py
+++ config_email.py
@@ -7,13 +7,16 @@ script_email_password = 'lxrhxzgwqurnvyqo'
 #                           'gusev.mihail.34@gmail.com': ('С2', 'АМ', 'ЗУ')
 #                           }
 
-batch_sending_journals = {'NMulin@en-techno.ru': ('АСУ ТП С1', 'АСУ И С1'),
-                          'VMakarov@bcc.ru': ('С2', 'АМ', 'ЗУ'),
-                          'APodolskij@en-techno.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
-                          'AIlin@en-techno.ru': ('ЛВС', 'ВОЛС', 'АИИСКУЕ'),
-                          'AYastrebov@en-techno.ru': ('Тех',),
-                          'AOgorodnikov@en-techno.ru': ('ТК',)
-                          }
+batch_sending_journals = {
+    'NMulin@en-techno.ru': ('АСУ ТП С1', 'АСУ И С1'),
+    'VHaritonov@bcc.ru': ('АСУ ТП С1 Север', 'АСУ И С1 Север'),
+    # 'MGusev@en-techno.ru': ('АСУ ТП С1 Юг', 'АСУ И С1 Юг'),
+    'VMakarov@bcc.ru': ('С2', 'АМ', 'ЗУ'),
+    'APodolskij@en-techno.ru': ('В1', 'В2', 'В3', 'В4', 'В5', 'В6'),
+    'AIlin@en-techno.ru': ('ЛВС', 'ВОЛС', 'АИИСКУЕ'),
+    'AYastrebov@en-techno.ru': ('Тех',),
+    'AOgorodnikov@en-techno.ru': ('ТК',)
+}
 
 batch_sending_plans = {'kzs.asu.504@gmail.com': ('',)}
 
